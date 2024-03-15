11129ce5723287c02fe09d368d82d50619e0263e
s28470 <s28470@pjwstk.edu.pl>
2024-03-15 22:42:23 +0100

    home page was created; home controller was created

--- todoApp/src/main/java/com/example/todoapp/TodoAppApplication.java
+++ todoApp/src/main/java/com/example/todoapp/TodoAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TodoAppApplication.class, args);
     }
